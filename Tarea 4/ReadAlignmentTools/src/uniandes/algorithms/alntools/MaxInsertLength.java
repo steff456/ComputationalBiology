791c7d07385461c70ca7e1dda8026514298d621f
steff456 <steff456@hotmail.com>
2018-10-28 02:56:00 -0500

    starting point 5

--- Tarea 4/ReadAlignmentTools/src/uniandes/algorithms/alntools/MaxInsertLength.java
+++ Tarea 4/ReadAlignmentTools/src/uniandes/algorithms/alntools/MaxInsertLength.java
@@ -1,5 +1,14 @@
 package uniandes.algorithms.alntools;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+
+import ngsep.alignments.ReadAlignment;
+import ngsep.alignments.io.ReadAlignmentFileReader;
+
 public class MaxInsertLength {
 		
 	/**
@@ -11,17 +20,48 @@ public class MaxInsertLength {
 		MaxInsertLength instance = new MaxInsertLength();
 		String temp = "";
 		int maxInsertLength = 0;
+		String outFile = "";
 		try {
 			temp = args[0];
 			maxInsertLength = Integer.parseInt(args[1]);
+			outFile = args[2];
 		}
 		catch(Exception e){
 			temp = "/Users/tefa/Documents/BioComp/Seg5_bowtie2_sorted.bam";			
 			maxInsertLength = 600;
 		}
+		instance.findAlnMaxLength(temp, maxInsertLength, outFile);
 //		instance.processAlignmentsFile(temp);
 //		instance.printStatistics(temp);
 		// Sequence name = cromosomas
+	}
+
+	/**
+	 * Find alignments with an insert length greater than the maximum given by parameter
+	 * Writes a file with the alignments found.
+	 * @param temp
+	 * @param maxInsertLength
+	 * @param outFile
+	 * @throws IOException 
+	 */
+	public void findAlnMaxLength(String file, int maxInsertLength, String outFile) throws IOException {
+		PrintWriter pw = new PrintWriter(new File(outFile));
+		pw.write("Running the program with the following parameters: \n");
+		pw.write("Input file: " + file + "\n");
+		pw.write("Maximum Length: " + maxInsertLength + "\n");
+		pw.write("------------------------------------------------------\n");
+		
+		//Reads input file
+		try (ReadAlignmentFileReader reader = new ReadAlignmentFileReader(file)){
+			Iterator<ReadAlignment> it = reader.iterator();
+			while (it.hasNext()) {
+				ReadAlignment aln = it.next();
+				// TODO: Check if the insert length is greater than the expected
+				pw.write("Sequence name: " + aln.getSequenceName()+ "\n");
+				pw.write("Final position: " + "\n");
+				pw.write("Initial position of mate: " + aln.getMateFirst() + "\n");
+			}
+		}
 	}
 
 }
